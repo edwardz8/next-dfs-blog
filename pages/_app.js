b2c19073cbe5bf2b503944c9d61e238a211a88a4
edwardz8 <tolklorist@gmail.com>
2020-12-02 18:24:54 -0500

    remove gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,6 @@
 import '../styles/global.css'
 import React from 'react'
-import { useRouter, useEffect } from 'next/router'
-import * as gtag from '../lib/gtag'
 
 export default function App({ Component, pageProps }) {
-    const router = useRouter()
-    useEffect(() => {
-        const handleRouteChange = () => {
-            gtag.pageview(url)
-        }
-        router.events.on('routeChangeComplete', handleRouteChange)
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange)
-        }
-    }, [router.events])
     return <Component {...pageProps} />
 }
